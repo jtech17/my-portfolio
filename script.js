3c610dc8f0f6fea52761d66047fdf070668b1736
jtech17 <frankjeremiah809@gmail.com>
2025-02-18 07:09:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const darkModeToggle = document.getElementById("dark-mode-toggle");
-  const body = document.body;
-
-  // Check if dark mode is enabled in local storage
-  if (localStorage.getItem("darkMode") === "enabled") {
-      body.classList.add("dark-mode");
+// Sticky Navigation Menu
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+// Show/hide sticky navigation and scroll button based on scroll position
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  } else {
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
   }
+};
 
-  darkModeToggle.addEventListener("click", () => {
-      body.classList.toggle("dark-mode");
-      
-      // Save preference in local storage
-      if (body.classList.contains("dark-mode")) {
-          localStorage.setItem("darkMode", "enabled");
-      } else {
-          localStorage.removeItem("darkMode");
-      }
-  });
+// Side Navigation Menu
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+// Open side navigation
+menuBtn.onclick = function () {
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+};
+const hideNavMenu = () => {
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+};
+// Close side navigation
+cancelBtn.onclick = hideNavMenu;
+// Close side navigation when a menu link is clicked
+let navLinks = document.querySelectorAll(".menu li a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", hideNavMenu);
 });
